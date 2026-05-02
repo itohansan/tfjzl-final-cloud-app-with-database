ef883e7579a4d67b922e159e9070d19b311c291d
itohansan <itohanidubor@gmail.com>
2026-05-02 06:47:33 -0400

    final update

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -137,5 +137,6 @@ def show_exam_result(request, course_id, submission_id):
     context['course'] = course
     context['grade'] = total_score
     context['choices'] = choices
+    context['questions'] = questions
 
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
